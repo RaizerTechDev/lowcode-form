3fbe5ad99c27c6483f68335c85c3aae9c109d994
RaizerTechDev <rafaelraizer76@gmail.com>
2025-07-07 19:20:14 +0100

    Update core functionality and dependecie

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '../public'))); public
+app.use(express.static(path.join(__dirname, '../public'))); 
 
 // Rota principal
 app.get('/', (req, res) => {
